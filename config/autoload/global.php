c4be5daa6b4be5571450a060218953c11b1b6871
kingavin <craftgavin@gmail.com>
2015-03-31 12:27:55 +0800

    added config

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,4 +1,15 @@
 <?php
 return array(
-	
+	'env' => array(
+		'usage' => 'production',
+		'base_path' => '/var/www',
+		'path' => array(
+			'fucms' => 'http://lib-media.qiniudn.com/fucms',
+			'src'	=> 'http://lib-media.qiniudn.com/src',
+			'compact' => 'http://lib-media.qiniudn.com/compact',
+			'aliyun' => 'http://en-developer.oss-cn-hangzhou.aliyuncs.com',
+			'qiniu' => 'http://en-developer.qiniudn.com',
+			'upyun' => 'http://en-developer.b0.upaiyun.com'
+		),
+	),
 );
