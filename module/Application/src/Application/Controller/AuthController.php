d4efcfe395913841ffcb3b91730a6daf6a36dde9
yf <5691549@qq.com>
2015-04-02 13:59:32 +0800

    bug fix

--- module/Application/src/Application/Controller/AuthController.php
+++ module/Application/src/Application/Controller/AuthController.php
@@ -22,7 +22,7 @@ class AuthController extends AbstractActionController
     {
     	$config = $this->getServiceLocator()->get('Config');
     	$wx = $config['env']['wx'];
-    	
+    	$dm = $this->documentManager();
     	$q = $this->params()->fromQuery();
     	$authCode = $q['auth_code'];
     	
@@ -30,8 +30,48 @@ class AuthController extends AbstractActionController
     		'component_appid' => $wx['appId'],
     		'authorization_code' => $authCode,
     	);
+    	
+    	$doc = $dm->createQueryBuilder('Application\Document\Admin')
+			    	->field('appSecret')->equals($wx['appSecret'])
+			    	->getQuery()
+			    	->getSingleResult();
+    	$ticket = $doc->getTicket();
+    	if(!$doc->getAccessToken()){
+    		$tokenFailed = true;
+    	}else {
+    		$modified = $doc->getModified()->format('y-m-d H:i:s');
+    		$cTimestamp = strtotime (date("y-m-d H:i:s"));
+    		$timestamp = strtotime ($modified);
+    		if($cTimestamp - $timestamp > 7200){
+    			$tokenFailed = true;
+    		}else {
+    			$accessToken = $doc->getAccessToken();
+    		}
+    	}
+    	if($tokenFailed) {
+    		$getTokenUrl = $wx['path']['accessToken'];
+    		$post_data = array (
+    			"component_appid" => $wx['appId'],
+    			"component_appsecret" =>$wx['appSecret'],
+    			'component_verify_ticket' => $ticket,
+    		);
+    		$post_data = json_encode($post_data);
+    		$tokenResultStr = $this->curlPostResult($getTokenUrl, $post_data);
+    	
+    		$tokenResult = json_decode($tokenResultStr , true);
+    	
+    		$currentDateTime = new \DateTime();
+    		$doc->setTokenModified($currentDateTime);
+    		$doc->setData(array(
+    			'tokenResult'=> $tokenResultStr,
+    		));
+    		$accessToken = $tokenResult['component_access_token'];
+    		$doc->setAccessToken($accessToken);
+    		$dm->persist($doc);
+    		$dm->flush();
+    	}
     	$post_data = json_encode($post_data);
-    	$getAuthInfoUrl = $wx['path']['authInfo'];
+    	$getAuthInfoUrl = $wx['path']['authInfo'].$accessToken;
 
     	$authInfoResultStr = $this->curlPostResult($getAuthInfoUrl, $post_data);
     	$authInfoResult = json_decode($authInfoResultStr, true);
