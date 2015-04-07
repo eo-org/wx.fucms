cdb2c8a499bd7a54897ac87f8c7b2b57bf05de32
yf <5691549@qq.com>
2015-04-07 22:35:06 +0800

    a

--- module/Application/src/Application/Controller/CallbackController.php
+++ module/Application/src/Application/Controller/CallbackController.php
@@ -191,9 +191,9 @@ class CallbackController extends AbstractActionController
     					}
     					$returnData['MsgType'] = $matchData['type'];
     				}else {
-    					$returnData['Content'] = '热烈欢迎您mo-鼓掌mo-鼓掌mo-鼓掌关注武汉长江联合官方微信账号，我们只提供领先的信息化解决方案，如果您对建站有任何的疑问，可随时咨询，我们将及时报以最专业的答复，您的十分满意是我们唯一的服务宗旨mo-得意~~';
+    					$returnData['Content'] = '热烈欢迎您/:handclap/:handclap/:handclap鼓掌关注武汉长江联合官方微信账号，我们只提供领先的信息化解决方案，如果您对建站有任何的疑问，可随时咨询，我们将及时报以最专业的答复，您的十分满意是我们唯一的服务宗旨mo-得意~~';
     					$returnData['MsgType'] = 'text';
-    				}    				
+    				}
     				break;
     			case 'image':
     				$picUrl = $postObj->PicUrl;
