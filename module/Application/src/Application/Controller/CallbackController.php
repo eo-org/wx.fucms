b66e1aefba5ba27deedd2d1591ce523b6d788b45
yf <5691549@qq.com>
2015-04-08 19:56:35 +0800

    bug fix6

--- module/Application/src/Application/Controller/CallbackController.php
+++ module/Application/src/Application/Controller/CallbackController.php
@@ -156,13 +156,13 @@ class CallbackController extends AbstractActionController
     			case 'text':
     				$content = $postObj->Content;
     				$messageData['content'] = $content;
-    				$keywordsDoc = $cdm->createQueryBuilder('Application\Document\Query')
-					    				->field('keywords')->equals($content)
-					    				->getQuery()->getSingleResult();
+//     				$keywordsDoc = $cdm->createQueryBuilder('Application\Document\Query')
+// 					    				->field('keywords')->equals($content)
+// 					    				->getQuery()->getSingleResult();
 					    				
     				$messageData['data'] = array(
     					'key' => $content,
-    					'sss' => $keywordsDoc->getContent(),
+//     					'sss' => $keywordsDoc->getContent(),
     				);
     				$matchData = '';
 //     				if($keywordsDoc) {
