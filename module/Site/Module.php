3ee91beec1c5d29dafe4e7567b1247806cfad656
kingavin <craftgavin@gmail.com>
2015-04-09 18:20:43 +0800

    addded component_id

--- module/Site/Module.php
+++ module/Site/Module.php
@@ -37,7 +37,7 @@ class Module
     	if($matchedRouteName == 'site') {
     		//header("Location: https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxf0e81c3bee622d60&redirect_uri=http%3A%2F%2Fnba.bluewebgame.com%2Foauth_response.php&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect");
     		
-    		header("Location: https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx536a9272e58807e7&redirect_uri=http://wxs.fucmsweb.com/get-user-code&response_type=code&scope=snsapi_base&state=gavin#wechat_redirect");
+    		header("Location: https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx536a9272e58807e7&redirect_uri=http%3A%2F%2Fwxs.fucmsweb.com%2Fget-user-code&response_type=code&scope=snsapi_base&state=gavin&component_appid=wx2ce4babba45b702d#wechat_redirect");
     		exit(0);
     	}
     }
