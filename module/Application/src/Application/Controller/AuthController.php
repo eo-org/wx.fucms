21b77d613046452ed51c7188d15252af9864f6e1
yf <5691549@qq.com>
2015-05-21 09:41:22 +0800

    small

--- module/Application/src/Application/Controller/AuthController.php
+++ module/Application/src/Application/Controller/AuthController.php
@@ -34,54 +34,11 @@ class AuthController extends AbstractActionController
     		'component_appid' => $wx['appId'],
     		'authorization_code' => $authCode,
     	);
+    	$pa = $sm->get('Application\Service\PublicityAuth');
+    	$componentAccessToken = $pa->getComponentAccessToken();
     	
-    	$tokenDoc = $dm->createQueryBuilder('Application\Document\Ticket')
-			    		->field('label')->equals('token')
-			    		->getQuery()
-			    		->getSingleResult();
-    	$tokenFailed = true;
-    	if(!empty($tokenDoc)) {
-    		    		
-    		$modified = $tokenDoc->getModified()->format('y-m-d H:i:s');
-    		$cTimestamp = strtotime (date("y-m-d H:i:s"));
-    		$timestamp = strtotime ($modified);
-    		if($cTimestamp - $timestamp < 6000){
-    			$token = $tokenDoc->getValue();
-    			$tokenFailed = false;
-    		}
-    	}else {
-    		$tokenDoc = new Ticket();
-    		$tokenDoc->setLabel('token');
-    	}
-    	if($tokenFailed) {
-    		$ticketDoc = $dm->createQueryBuilder('Application\Document\Ticket')
-				    		->field('label')->equals('ticket')
-				    		->getQuery()
-				    		->getSingleResult();
-    		$ticket = $ticketDoc->getValue();
-    		
-    		$getTokenUrl = $wx['path']['accessToken'];
-    		$post_data = array (
-    			"component_appid" => $wx['appId'],
-    			"component_appsecret" =>$wx['appSecret'],
-    			'component_verify_ticket' => $ticket,
-    		);
-    		$post_data = json_encode($post_data);
-    		$tokenResultStr = $this->curlPostResult($getTokenUrl, $post_data);
-    		$tokenResult = json_decode($tokenResultStr , true);
-    		$currentDateTime = new \DateTime();
-    		$tokenDoc->setMsg(array(
-    			'tokenResult' => $tokenResultStr,
-    		));
-    		
-    		$token = $tokenResult['component_access_token'];
-    		$tokenDoc->setValue($token);
-    		$tokenDoc->setModified($currentDateTime);
-    		$dm->persist($tokenDoc);
-    		$dm->flush();
-    	}
     	$post_data = json_encode($post_data);
-    	$getAuthInfoUrl = $wx['path']['authInfo'].$token;
+    	$getAuthInfoUrl = $wx['path']['authInfo'].$componentAccessToken;
 
     	$authInfoResultStr = $this->curlPostResult($getAuthInfoUrl, $post_data);
     	$authInfoResult = json_decode($authInfoResultStr, true);
@@ -103,9 +60,8 @@ class AuthController extends AbstractActionController
     	$authDoc->setCreated($currentDateTime);
     	$dm->persist($authDoc);
     	$dm->flush();
-    	$pa = $sm->get('Application\Service\PublicityAuth');
-    	$componentAccessToken = $pa->getComponentAccessToken();
     	
+    	$componentAccessToken = $pa->getComponentAccessToken();    	
     	$getAuthorizerInfoUrl = 'https://api.weixin.qq.com/cgi-bin/component/api_get_authorizer_info?component_access_token='.$componentAccessToken;
     	$postData = array(
     		'component_appid' => $wx['appId'],
