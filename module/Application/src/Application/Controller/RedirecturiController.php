9c245b2e03206cd3426b081b6b96938a448668bc
yf <5691549@qq.com>
2015-11-23 17:39:14 +0800

    预授权bug

--- module/Application/src/Application/Controller/RedirecturiController.php
+++ module/Application/src/Application/Controller/RedirecturiController.php
@@ -22,62 +22,18 @@ class RedirecturiController extends AbstractActionController
 	
     public function indexAction()
     {
-    	$dm = $this->getServiceLocator()->get('DocumentManager');
+    	$sm = $this->getServiceLocator();
+    	$dm = $sm->get('DocumentManager');
     	
     	$config = $this->getServiceLocator()->get('Config');
     	$wx = $config['env']['wx'];
-    	
-    	
-    	$tokenDoc = $dm->createQueryBuilder('Application\Document\Ticket')
-				    		->field('label')->equals('token')
-				    		->getQuery()
-				    		->getSingleResult();
-    	$tokenFailed = true;
-    	if(!empty($tokenDoc)) {
-    		    		
-    		$modified = $tokenDoc->getModified()->format('y-m-d H:i:s');
-    		$cTimestamp = strtotime (date("y-m-d H:i:s"));
-    		$timestamp = strtotime ($modified);
-    		if($cTimestamp - $timestamp < 6000){
-    			$token = $tokenDoc->getValue();
-    			$tokenFailed = false;
-    		}
-    	}else {
-    		$tokenDoc = new Ticket();
-    		$tokenDoc->setLabel('token');
-    	}
-    	if($tokenFailed) {
-    		$ticketDoc = $dm->createQueryBuilder('Application\Document\Ticket')
-				    		->field('label')->equals('ticket')
-				    		->getQuery()
-				    		->getSingleResult();
-    		
-    		$ticket = $ticketDoc->getValue();
-    		$getTokenUrl = $wx['path']['accessToken'];
-    		$post_data = array (
-    			"component_appid" => $wx['appId'],
-    			"component_appsecret" =>$wx['appSecret'],
-    			'component_verify_ticket' => $ticket,
-    		);
-    		$post_data = json_encode($post_data);
-    		$tokenResultStr = $this->curlPostResult($getTokenUrl, $post_data);
-    		$tokenResult = json_decode($tokenResultStr , true);
-    		$currentDateTime = new \DateTime();
-    		$tokenDoc->setMsg(array(
-    			'tokenResult' => $tokenResultStr,
-    		));
-    		
-    		$token = $tokenResult['component_access_token'];
-    		$tokenDoc->setValue($token);
-    		$tokenDoc->setModified($currentDateTime);
-    		$dm->persist($tokenDoc);
-    		$dm->flush();
-    	}
+    	$pa = $sm->get('Application\Service\PublicityAuth');
+    	$accessToken = $pa->getComponentAccessToken();
  		$preAuthCodePostData = array(
  			'component_appid' => $wx['appId'],
  		);
  		$preAuthCodePostDataStr = json_encode($preAuthCodePostData);
- 		$getPreAuthCodeUrl = $wx['path']['preAuthCode'].$token;
+ 		$getPreAuthCodeUrl = $wx['path']['preAuthCode'].$accessToken;
  		
  		$preAuthCodeResultStr = $this->curlPostResult($getPreAuthCodeUrl, $preAuthCodePostDataStr);
  		$preAuthCodeResult = json_decode($preAuthCodeResultStr , true);
